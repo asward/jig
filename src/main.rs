0fc07aae9d6f424da71ccc8d5bd6214b313b3f6d
Andrew Ward <asward@ucdavis.edu>
2026-02-17 10:15:17 -0800

    init

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use clap::Parser;
+use mouse_rs::Mouse;
+use std::thread;
+use std::time::Duration;
+
+#[derive(Parser)]
+struct Args {
+    /// Idle timeout in seconds
+    #[arg(short = 'i', long, env = "JIG_INTERVAL", default_value = "60")]
+    timeout: Option<u64>,
+}
+
+fn main() {
+    let args = Args::parse();
+    let timeout = Duration::from_secs(args.timeout.unwrap());
+    let mouse = Mouse::new();
+    loop {
+        match mouse.get_position() {
+            Ok(position) => _ = mouse.move_to(position.x + 1, position.y + 1),
+            Err(e) => {
+                eprintln!("Error getting mouse position: {}", e);
+                continue;
+            }
+        }
+        thread::sleep(timeout);
+    }
+}
